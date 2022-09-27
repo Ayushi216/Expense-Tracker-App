4bef2d4ae9ecd42562d47772a3e8020cd51ddd38
Ayushi <ayushisarkar44@gmail.com>
2022-09-27 17:29:00 +0530

    email verification

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -17,6 +17,43 @@ const Welcome = (props) => {
       history.replace("/auth");
     };
 
+    const verifyHandler = (props) => {
+      fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyCm1BvBfb655cubTMVdda30otMpIuOclhk",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            requestType: 'VERIFY_EMAIL',
+            idToken: authCtx.token,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          } else {
+            return res.json().then((data) => {
+              let errorMessage = "Data could not be fetched";
+              if (data && data.error && data.error.message) {
+                  errorMessage = data.error.message;
+                }
+    
+              throw new Error(errorMessage);
+            });
+          }
+        })
+        .then((data) => {
+          console.log("Email is verified");
+          console.log(data.email);
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
+    }
+
   return (
     <Fragment>
       <section className={classes.section}>
@@ -24,6 +61,10 @@ const Welcome = (props) => {
         <p className={classes.p}> You profile is incomplete!</p> 
         <button className={classes.button} onClick={onClickHandler}> Complete now </button>
         <button className={classes.logout} onClick = {logoutHandler}> Logout </button>
+      </section>
+      <section className={classes.verify}>
+        <p> Verify your email now!</p>
+        <button className={classes.verifybutton}  onClick={verifyHandler}> Click here!</button>
       </section>
     </Fragment>
   );
